42aa7a5787f1c16991939ff715dbbc8702d36ea6
jeremyruizacevedo <jeremy93fra@gmail.com>
2021-06-22 17:34:09 -0500

    add variable type to test.yml

--- .github/workflows/actions/entrypoint.sh
+++ .github/workflows/actions/entrypoint.sh
@@ -153,19 +153,21 @@ echo "INPUT_TYPE: $INPUT_TYPE"
 
 echo "
 prod_gcs:
-  type: '$INPUT_TYPE'
-  project_id: 'ecosystem-dev'
-  private_key_id: 'private_key_id'
-  private_key: '-----BEGIN PRIVATE KEY-----\nMIZutUqafI=\n-----END PRIVATE KEY-----\n'
-  client_email: 'client_email'
-  client_id: 'client_id'
-  auth_uri: 'auth_uri'
-  token_uri: 'token_uri'
-  auth_provider_x509_cert_url: 'auth_provider_x509_cert_url'
-  client_x509_cert_url: 'hclient_x509_cert_url'
+  type: \"$INPUT_TYPE\"
+  project_id: "ecosystem-dev"
+  private_key_id: "private_key_id"
+  private_key: "-----BEGIN PRIVATE KEY-----\nMIZutUqafI=\n-----END PRIVATE KEY-----\n"
+  client_email: "client_email"
+  client_id: "client_id"
+  auth_uri: "auth_uri"
+  token_uri: "token_uri"
+  auth_provider_x509_cert_url: "auth_provider_x509_cert_url"
+  client_x509_cert_url: "hclient_x509_cert_url"
 
 email_secret_id: email_secret_id
+
 " > ./conf/base/credentials.yml
+
 ls ./conf/base
 cat ./conf/base/credentials.yml
 
